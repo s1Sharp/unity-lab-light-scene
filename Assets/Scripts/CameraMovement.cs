7c40a8428efcd9d87d2b81e20a9a95b3edb61f59
Maksim "s1Sharp" Tsarkov <s1sharp.it@gmail.com>
2023-03-19 05:25:01 +0300

    init simple task for unity scene

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+    [SerializeField] private float distanceToTarget = 10;
+
+    private Vector3 previousPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
+
+            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cameraPoint1;
+    [SerializeField]
+    GameObject cameraPoint2;
+    [SerializeField]
+    GameObject cameraPoint3;
+    [SerializeField]
+    GameObject cameraPoint4;
+    [SerializeField]
+    Light[] lights;
+
+    private Transform camera;
+
+    private void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+    public void OnCam1Click()
+    {
+        camera.transform.position = cameraPoint1.transform.position;
+        camera.transform.rotation = cameraPoint1.transform.rotation;
+    }
+
+    public void OnCam2Click()
+    {
+        camera.transform.position = cameraPoint2.transform.position;
+        camera.transform.rotation = cameraPoint2.transform.rotation;
+    }
+
+    public void OnCam3Click()
+    {
+        camera.transform.position = cameraPoint3.transform.position;
+        camera.transform.rotation = cameraPoint3.transform.rotation;
+    }
+
+    public void OnCam4Click()
+    {
+        camera.transform.position = cameraPoint4.transform.position;
+        camera.transform.rotation = cameraPoint4.transform.rotation;
+    }
+
+    public void OnReverseLightClick()
+    {
+        foreach (var l in lights)
+            l.gameObject.SetActive(!l.gameObject.activeSelf);
+    }
+
+    public void OnLamp1ValueChange(float val)
+    {
+        lights[0].intensity = val;
+    }
+
+    public void OnLamp2ValueChange(float val)
+    {
+        lights[1].intensity = val;
+    }
+
+    public void OnLamp3ValueChange(float val)
+    {
+        lights[2].intensity = val;
+    }
+
+    public void OnLamp4ValueChange(float val)
+    {
+        lights[3].intensity = val;
+    }
+
+    public void OnLamp5ValueChange(float val)
+    {
+        // table lamp changes
+        lights[4].intensity = val;
+        lights[5].intensity = val;
+    }
+}
